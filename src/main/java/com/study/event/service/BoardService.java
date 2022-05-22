5f65dcb265df770db8c1c5dbd86de3deedc0c92f
이승민 <sml7179@naver.com>
2022-05-22 16:40:51 +0900

    feat: 게시판 조회 기능 추가

--- src/main/java/com/study/event/service/BoardService.java
+++ src/main/java/com/study/event/service/BoardService.java
@@ -6,8 +6,10 @@ import com.study.event.dtos.BoardRequest;
 import com.study.event.repository.BoardRepository;
 import com.study.event.repository.HistoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class BoardService {
 
     private final BoardRepository boardRepository;
@@ -18,10 +20,16 @@ public class BoardService {
         this.historyRepository = historyRepository;
     }
 
+    @Transactional
     public long createNotEvent(BoardRequest boardRequest){
         Board createBoard = boardRequest.toBoard();
         Board savedBoard = boardRepository.save(createBoard);
         historyRepository.save(new History(createBoard));
         return savedBoard.getId();
+    }
+
+    public Board findBoardById(long id) {
+        return boardRepository.findById(id)
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
--- src/main/java/com/study/event/ui/BoardController.java
+++ src/main/java/com/study/event/ui/BoardController.java
@@ -1,11 +1,10 @@
 package com.study.event.ui;
 
+import com.study.event.domain.Board;
 import com.study.event.dtos.BoardRequest;
 import com.study.event.service.BoardService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -18,10 +17,17 @@ public class BoardController {
         this.boardService = boardServiceBefore;
     }
 
-    @PostMapping("/board")
+    @GetMapping("/{id}")
+    public ResponseEntity<Board> findBoardById(@PathVariable long id){
+        return ResponseEntity.ok(boardService.findBoardById(id));
+    }
+
+    @PostMapping("/board/v1")
     public ResponseEntity<Void> createBoardNotEvent(@RequestBody BoardRequest boardRequest) {
         long boardId = boardService.createNotEvent(boardRequest);
         URI uri = URI.create("/board"+boardId);
         return ResponseEntity.created(uri).body(null);
     }
+
+
 }
