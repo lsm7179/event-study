3b880794bf424085f7d13b06282b230997d32743
이승민 <sml7179@naver.com>
2022-05-22 17:24:05 +0900

    feat: GeneratedValue 속성 변경

--- src/main/java/com/study/event/domain/Board.java
+++ src/main/java/com/study/event/domain/Board.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class Board extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
--- src/main/java/com/study/event/domain/History.java
+++ src/main/java/com/study/event/domain/History.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 public class History extends BaseTimeEntity{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne
