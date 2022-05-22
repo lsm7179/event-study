f39e01e22b31e3185894491e8b503c5d2cf6d5a3
이승민 <sml7179@naver.com>
2022-05-22 17:36:49 +0900

    fix: 이벤트 리스너 안먹던 현상 수정

--- src/main/java/com/study/event/event/BoardEventListener.java
+++ src/main/java/com/study/event/event/BoardEventListener.java
@@ -5,11 +5,13 @@ import com.study.event.domain.Board;
 import com.study.event.domain.History;
 import com.study.event.repository.BoardRepository;
 import com.study.event.repository.HistoryRepository;
-import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Transactional(propagation = Propagation.REQUIRES_NEW)
+@Component
 public class BoardEventListener {
 
     private final BoardRepository boardRepository;
@@ -20,7 +22,7 @@ public class BoardEventListener {
         this.historyRepository = historyRepository;
     }
 
-    @EventListener
+    @TransactionalEventListener
     public void createBoardEvent(CreatedBoard createdBoard) {
         Board board = boardRepository.findById(createdBoard.getBoardId())
                 .orElseThrow(IllegalArgumentException::new);
