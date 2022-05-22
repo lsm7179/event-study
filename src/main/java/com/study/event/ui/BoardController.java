734be5374f341149b880a224d22e0ecb67fe4697
이승민 <sml7179@naver.com>
2022-05-22 17:09:54 +0900

    feat: APPLICATION_JSON_VALUE 추가

--- src/main/java/com/study/event/ui/BoardController.java
+++ src/main/java/com/study/event/ui/BoardController.java
@@ -3,6 +3,7 @@ package com.study.event.ui;
 import com.study.event.domain.Board;
 import com.study.event.dtos.BoardRequest;
 import com.study.event.service.BoardService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,14 +23,14 @@ public class BoardController {
         return ResponseEntity.ok(boardService.findBoardById(id));
     }
 
-    @PostMapping("/board/v1")
+    @PostMapping(value = "/board/v1", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createBoardNotEvent(@RequestBody BoardRequest boardRequest) {
         long boardId = boardService.createNotEvent(boardRequest);
         URI uri = URI.create("/board"+boardId);
         return ResponseEntity.created(uri).body(null);
     }
 
-    @PostMapping("/board/v2")
+    @PostMapping(value = "/board/v2", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createBoardWithEvent(@RequestBody BoardRequest boardRequest) {
         long boardId = boardService.createWithEvent(boardRequest);
         URI uri = URI.create("/board"+boardId);
